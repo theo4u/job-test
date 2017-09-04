4b73c3738de8f81ad8f2ea978a1244fe754b0f09
Theophilus Omorebee <theo4u@ymail.com>
2017-09-04 13:47:36 +0100

    done with getTopBooks.

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -1,5 +1,6 @@
 const books = require('./books');
 let fetchBookByAuthors = require('./fetch-books-by-authors');
+let ratings = require('./ratings');
 
 const print = result => console.log('Result:', result);
 
@@ -21,10 +22,23 @@ const getBooksAsync = (callback) => {
     }, 2000)
 };
 
+// testing fetch book by author .js
 const getBooksByAuthor = (author, callback) => {
     return callback(fetchBookByAuthors(author));
 };
 
+// processing ratings.js
+let Rating = ratings(books);
+const getTopBooks = (n, callback) => {
+    return callback(Rating.getTopBooks(n));
+};
+const getTopAuthors = (n, callback) => {
+    return callback(Rating.getTopAuthors(n));
+};
+
+
 //getBooksAsync(sortBooksAlphabetically);
 
 getBooksByAuthor('George Orwell', print);
+getTopBooks(5, print);
+getTopAuthors(5, print);
---
+++ javascript/ratings.js
@@ -0,0 +1,43 @@
+/**
+ * Created by theophy on 04/09/2017.
+ *
+ * a module named  Ratings  which accepts a collection of books, orders the
+ * books by ratings; Highest ratings first, lowest ratings last. This module should then expose
+ * two public functions; (1) getTopBooks: Accepts an integer nth  and returns the first nth books in the
+ * sorted collection.  (2) getTopAuthors: accepts an integer nth and returns an Array of objects with name
+ * of author as keys and their average ratings as values.
+ */
+
+/**
+ * this takes in the books and then sort it based on the sort other
+ * @param books array
+ * @param sort_order {'ASC'|'DESC'} default it takes highest to lowest which is 'DESC'
+ * @return {{getTopBooks: (function()), getTopAuthors: (function())}}
+ */
+module.exports = (books, sort_order) => {
+    sort_order = sort_order || 'DESC'; //use DESC as our default sorting order
+
+    let sortedBooks = books.sort((book1, book2) => {
+        return sort_order === 'DESC' ? (book2.ratings - book1.ratings) : (book1.ratings - book2.ratings);
+    });
+
+    return {
+        /**
+         * Accepts an integer nth  and returns the first nth books in the sorted collection
+         * @param n
+         * @return {Array}
+         */
+        getTopBooks: (n) => {
+            return sortedBooks.slice(0, n);
+        },
+        /**
+         * accepts an integer nth and returns an Array of objects with name of author
+         * as keys and their average ratings as values
+         * @param n
+         * @return {{}}
+         */
+        getTopAuthors: (n) => {
+            return {};
+        }
+    }
+};
